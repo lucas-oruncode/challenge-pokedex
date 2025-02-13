532b1ea61bb6aa998ae1ea20cc73a8cf7d79f304
Lucas de Jesus <oruncode@gmail.com>
2025-02-13 09:57:01 -0300

    feat: create Title

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -1,10 +1,12 @@
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import Title from "./Title";
 
 export default function HomeLayout() {
     return (
         <div>
            <NavBar />
+           <Title />
            <Footer />
         </div>
     );
