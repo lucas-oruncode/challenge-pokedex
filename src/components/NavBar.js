1bb25529637e01ed896379d2a05278f309178def
Lucas de Jesus <oruncode@gmail.com>
2025-02-13 09:39:20 -0300

    feat(components) : create Layout, Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <div className="bg-red-700/80 min-w-[100%] p-3 flex justify-center">
+                <img 
+                className="max-w-250 max-h-10" 
+                src="/banner.png" 
+                alt="Home" 
+                />
+        </div>
+    );
+}
