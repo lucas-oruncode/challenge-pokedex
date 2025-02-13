4995d9e11d1faf6d86ee3ba356b890b65631cc16
Lucas de Jesus <oruncode@gmail.com>
2025-02-13 11:43:17 -0300

    feat: add props on Title

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -3,11 +3,15 @@ import Footer from "./Footer";
 import NavBar from "./NavBar";
 import Title from "./Title";
 
+const titleInfo = {
+    title: 'Welcome to Pokedex',
+    subtitle: 'Click on the cards to see more details'
+}
 export default function HomeLayout() {
     return (
         <div className="min-h-screen w-full bg-[url(/bg-img.jpg)] bg-cover bg-center bg-fixed flex flex-col items-center justify-between">
            <NavBar />
-           <Title />
+           <Title title={titleInfo.title} subtitle={titleInfo.subtitle}/>
            <CardList />
            <Footer />
         </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,8 @@
-export default function Title() {
+export default function Title({ title, subtitle }) {
     return (
         <div className="flex flex-col p-10 border border-white/80 rounded-xl items-center justify-center bg-black/90">
-            <h1 className="text-2xl md:text-3xl text-white font-bold">Title</h1>
-            <p className="text-1xl text-white">subtitle</p>
+            <h1 className="text-2xl md:text-3xl text-white font-bold">{title}</h1>
+            <p className="text-1xl text-white">{subtitle}</p>
         </div>
     );
 }
