3cba35d9ded976f315613f430917e80f7b658ca6
Lucas de Jesus <oruncode@gmail.com>
2025-02-13 11:48:30 -0300

    feat(NavBar): adjust margin bottom

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 export default function NavBar() {
     return (
-        <div className="bg-red-700/80 min-w-[100%] p-3 flex justify-center">
+        <div className="bg-red-700/80 min-w-[100%] p-3 flex justify-center mb-10">
                 <img 
                 className="max-w-250 max-h-10" 
                 src="/banner.png" 
