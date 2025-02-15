3ffb149525f033ae8cec3c21c754c755a1cede52
Lucas de Jesus <oruncode@gmail.com>
2025-02-15 16:31:06 -0300

    feat(style): adjust back button margin

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function BackButton({ pathToReturn }) {
     return (
-        <Link href={pathToReturn} className=" px-6 py-3 bg-red-700/80 hover:bg-red-700 text-white font-semibold rounded-full shadow-md transition-transform hover:scale-105">
+        <Link href={pathToReturn} className="mt-5 px-6 py-3 bg-red-700/80 hover:bg-red-700 text-white font-semibold rounded-full shadow-md transition-transform hover:scale-105">
             ← Voltar
         </Link>
     );
